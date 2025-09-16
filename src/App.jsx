12650a272a9449d0d68b40f3a25774b2e8bc90ec
Bhabin Dulal <bhabindulal35@gmail.com>
2025-09-16 20:51:17 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { NotFound } from './pages/NotFound'
 import { Home } from './pages/Home'
+import { Toaster } from './components/ui/toaster'
 
 function App() {
   return (
    <>
+   <Toaster/>
    <BrowserRouter>
    <Routes>
       <Route index element = {<Home/>}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const navItems = [
     {name: "About", href: "#about"},
     {name: "Skills", href: "#skills"},
     {name: "Projects", href: "#projects"},
-    {name: "Contacts", href: "#contact"},    
+    {name: "Contact", href: "#contact"},    
 ]
 
 function Navbar() {
